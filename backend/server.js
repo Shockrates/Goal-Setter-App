6c8f77f14c244f2744e359eddac9d279adb2f6df
ShockRate <blackhound05@yahoo.gr>
2022-03-20 11:40:50 +0200

    Heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
-
+import path from 'path';
+import {fileURLToPath} from 'url';
 import goalRoutes from './routes/goalRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import  {errorHandler} from './middleware/errorMiddleware.js';
@@ -9,6 +10,10 @@ import connectDB from './config/db.js';
 
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const port = process.env.PORT || 5000;
 connectDB();
 
@@ -20,6 +25,15 @@ app.use(express.urlencoded({ extended: false}))
 
 app.use('/api/goals/', goalRoutes);
 app.use('/api/users/', userRoutes);
+
+//Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../','frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler);
 
